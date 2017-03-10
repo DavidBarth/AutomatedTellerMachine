2d436fd57853d83283c95a63bc4e7032da702b7e
David Barth <davidbarth80@gmail.com>
2017-03-10 18:45:18 +0000

    Create Model, View and Conroller, test data supplied to Details Action in CheckingAccountController

---
+++ AutomatedTellerMachine/Controllers/CheckingAccountController.cs
@@ -0,0 +1,95 @@
+﻿using AutomatedTellerMachine.Models;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class CheckingAccountController : Controller
+    {
+        // GET: CheckingAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CheckingAccount/Details/5
+        public ActionResult Details() //working from memory currently, hence no 'int id'
+        {
+            //supplying test data and passing it to view
+            var testData = new CheckingAccount
+            {
+                
+                AccountNumber = "00001234",
+                FirstName = "David",
+                LastName = "Barth"
+            };
+
+            return View(testData);
+        }
+
+        // GET: CheckingAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace AutomatedTellerMachine.Models
+{
+    public class CheckingAccount
+    {
+        public int AccountId { get; set; }
+        public string AccountNumber { get; set; }
+        public string FirstName  { get; set; }
+        public string LastName { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
