8bbff04476a62949fc6a25cdb5a38514bd86fff6
David Barth <davidbarth80@gmail.com>
2017-03-13 18:06:08 +0000

    Data annotations for client side validation for create form, use string interpolation and expression body for Name property

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -1,11 +1,33 @@
-﻿namespace AutomatedTellerMachine.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutomatedTellerMachine.Models
 {
     public class CheckingAccount
     {
-        public int AccountId { get; set; }
+        //primary key
+        public int Id { get; set; }
+
+        [RegularExpression(@"\d{6,10}", ErrorMessage = "Account number must be between 6 and 10 digits.")]
+        [Display(Name="Account Number")]
         public string AccountNumber { get; set; }
+
+        [Display(Name="First Name")]
         public string FirstName  { get; set; }
+
+        [Display(Name="Last Name")]
         public string LastName { get; set; }
+
+        /*public string Name 
+         * {
+         *    get
+         *    {
+         *      return string.Format("{0} {1}", this.firstName, this.lastName);
+         *    }
+         * }
+         * */
+        public string Name => $"{FirstName} {LastName}"; //combines first and last name
+
+        [DataType(DataType.Currency)]
         public decimal Balance { get; set; }
     }
 }
