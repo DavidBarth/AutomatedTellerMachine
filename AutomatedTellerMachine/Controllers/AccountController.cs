aa7a05e121ec3d73ca4622427475187bd37f526b
David Barth <davidbarth80@gmail.com>
2017-03-14 04:04:51 +0000

    Register view updated - Register() in Account controller working

--- AutomatedTellerMachine/Controllers/AccountController.cs
+++ AutomatedTellerMachine/Controllers/AccountController.cs
@@ -149,7 +149,11 @@ namespace AutomatedTellerMachine.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
